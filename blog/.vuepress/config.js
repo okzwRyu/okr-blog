473bafdbe1f712890dfd0f6669297f76302ec5cb
Ryu Okazawa <rokazawa@it-brn.com>
2020-10-08 10:43:27 +0900

    Create vuepress blog format.

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: "OKR-BLOG",
+    theme: '@vuepress/theme-blog',
+    themeConfig: {
+      description: "たくさんの雑記とほんの少しのTipsを投稿します。",
+      nav: [
+        { text: "About", link: "/about/" }
+      ]
+    }
+  };
---
+++ blog/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+description: たくさんの雑記とほんの少しのTipsを投稿します。
+---
+
+# OKR-BLOG  
+トップページへようこそ  
