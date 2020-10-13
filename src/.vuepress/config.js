0f7ad407c04bc006bcd5c292de82b3ca0d7ca13d
Ryu Okazawa <rokazawa@it-brn.com>
2020-10-14 00:19:11 +0900

    Replace icon

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -12,15 +12,9 @@ module.exports = {
     personalInfo: {
       nickname: "OKR",
       description: "駆け出しSE",
-    //   email: "okr_1205@outlook.jp",
       location: "Tokyo, Japan",
       organization: "",
-      // Your avatar image
-      // Set to external link
-      // TODO: Add image file.
-      avatar: "https://www.meteorlxy.cn/assets/img/avatar.jpg",
-      // Or put into `.vuepress/public` directory. E.g. `.vuepress/public/img/avatar.jpg`
-      // avatar: '/img/avatar.jpg',
+      avatar: "/images/tousen_karo_icon.jpg",
 
       sns: {
         github: {
@@ -31,34 +25,19 @@ module.exports = {
     },
 
     header: {
-      // The background of the header. You can choose to use an image, or to use random pattern (geopattern)
       background: {
-        // URL of the background image. If you set the URL, the random pattern will not be generated, and the `useGeo` will be ignored.
-        //   url: '/assets/img/bg.jpg',
-
-        // Use random pattern. If you set it to `false`, and you don't set the image URL, the background will be blank.
         useGeo: true,
       },
-
-      // show title in the header or not
       showTitle: true,
     },
 
     footer: {
-      // Show 'Powered by VuePress' or not (enable it to support VuePress)
       poweredBy: true,
-
-      // Show the theme that you are using (enable it to support this theme) (please do not disable it, haha)
       poweredByTheme: true,
-
-      // Add your custom footer (HTML supported)
       custom: 'Copyright 2020-present <a href="https://github.com/okzwRyu" target="_blank">okzwRyu</a> | MIT License',
     },
 
-    // Show the last updated time of your posts
     lastUpdated: true,
-
-    // The content of your navbar links
     nav: [
       { text: "Home", link: "/", exact: true },
       { text: "Posts", link: "/posts/", exact: false },
@@ -82,12 +61,10 @@ module.exports = {
       clientSecret: "MY_CLIENT_SECRET",
     },
 
-    // Pagination config
     pagination: {
       perPage: 5,
     },
 
-    // Default Pages (Optional, the default value of all pages is `true`)
     defaultPages: {
       home: true,
       posts: true,
